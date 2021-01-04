320d5c9f4f7fe1de26964fe76cfd7de86de8f46d
theo.lvq <t.lvq22@gmail.com>
2021-01-04 15:04:27 +0100

    v2

---
+++ app/src/main/java/fr/istic/mob/bus/ui/fragments/FragmentOne.kt
@@ -0,0 +1,122 @@
+package fr.istic.mob.bus.ui.fragments
+
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import fr.istic.mob.bus.R
+import java.text.SimpleDateFormat
+import java.util.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FragmentOne.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FragmentOne : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment FragmentOne.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            FragmentOne().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val textViewDate: TextView = view.findViewById(R.id.textViewDate)
+        val textViewHeure: TextView = view.findViewById(R.id.textViewHeure)
+
+        textViewDate.setOnClickListener {
+            val calendar = Calendar.getInstance()
+
+            val dateSetListener =
+                DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                    calendar.set(Calendar.YEAR, year)
+                    calendar.set(Calendar.MONTH, monthOfYear)
+                    calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                    val myFormat = "dd/MM/yyyy"
+                    val sdf = SimpleDateFormat(myFormat, Locale.FRANCE)
+                    textViewDate.text = sdf.format(calendar.time)
+
+                }
+
+            textViewDate.setOnClickListener {
+                DatePickerDialog(
+                    this.requireContext(), dateSetListener,
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        }
+        textViewHeure.setOnClickListener {
+            val calendar = Calendar.getInstance()
+
+            val timeSetListener =
+                TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+                    calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                    calendar.set(Calendar.MINUTE, minute)
+
+                    val myFormat = "hh:mm"
+                    val sdf = SimpleDateFormat(myFormat, Locale.FRANCE)
+                    textViewHeure.text = sdf.format(calendar.time)
+
+                }
+
+            textViewHeure.setOnClickListener {
+                TimePickerDialog(
+                    this.requireContext(), timeSetListener,
+                    calendar.get(Calendar.HOUR_OF_DAY),
+                    calendar.get(Calendar.MINUTE),
+                    true
+                ).show()
+            }
+        }
+    }
+
+}
